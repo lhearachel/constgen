f450ca419141283e246099c566b9dfb70bd606ae
Rachel <lhearachel@proton.me>
2024-01-06 15:51:08 -0800

    + Meson integration

---
+++ constgen/constgen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from pathlib import Path
+
+from dump import dump_from_file
+from lang import Language
+
+def constgen():
+    ARG_P = ArgumentParser(
+        prog='constgen',
+        description='Multi-language constants-file generator'
+    )
+    ARG_P.add_argument('-f', '--file',
+                       dest='files',
+                       help='input manifest file(s) containing the consts schema',
+                       nargs='+')
+    ARG_P.add_argument('-r', '--root',
+                       help='root directory in which to dump generated files',
+                       default='.')
+    ARG_P.add_argument('-l', '--lang',
+                       dest='langs',
+                       help='language target for generated files',
+                       choices=[l.name.lower() for l in Language],
+                       nargs='+')
+    
+    args = ARG_P.parse_args()
+
+    target_root = Path(args.root)
+    for target_file in args.files:
+        dump_from_file(Path(target_file),
+                       list(map(lambda l: Language[l.upper()], args.langs)),
+                       target_root)
+
+
+if __name__ == '__main__':
+    constgen()
--- constgen/dump.py
+++ constgen/dump.py
@@ -1,4 +1,3 @@
-from argparse import ArgumentParser
 from pathlib import Path
 
 from lang import Language, LANG_EXTS, LANG_FUNCS
@@ -29,34 +28,3 @@ def dump(schema: Schema, langs: list[Language], root: Path=Path('.')):
 def dump_from_file(mapping: Path, langs: list[Language], root: Path=Path('.')):
     '''Convenience wrapper for dumping a schema defined in a manifest file.'''
     dump(Schema.from_file(mapping), langs, root)
-
-
-def constgen():
-    ARG_P = ArgumentParser(
-        prog='constgen',
-        description='Multi-language constants-file generator'
-    )
-    ARG_P.add_argument('-f', '--file',
-                       dest='files',
-                       help='input manifest file(s) containing the consts schema',
-                       nargs='+')
-    ARG_P.add_argument('-r', '--root',
-                       help='root directory in which to dump generated files',
-                       default='.')
-    ARG_P.add_argument('-l', '--lang',
-                       dest='langs',
-                       help='language target for generated files',
-                       choices=[l.name.lower() for l in Language],
-                       nargs='+')
-    
-    args = ARG_P.parse_args()
-
-    target_root = Path(args.root)
-    for target_file in args.files:
-        dump_from_file(Path(target_file),
-                       list(map(lambda l: Language[l.upper()], args.langs)),
-                       target_root)
-
-
-if __name__ == '__main__':
-    constgen()
